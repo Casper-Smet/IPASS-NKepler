15c65e5fc7f37290c192762a40ac19c3c28770a6
Casper Smet <casper.smet@student.hu.nl>
2019-06-19 21:47:34 +0200

    Changed initializer, simulated earth-mars around sun, moon around earth

--- classes.py
+++ classes.py
@@ -1,12 +1,12 @@
-from matplotlib import pyplot as plt
 from math import cos, sin
 from utility import gravitational_constant, pi, degrees, radians, range_setter, accuracy
 
 from functools import lru_cache
 
 
-# Todo Remove real_distance, initialize with radius
-
+# Todo Write my own sin and cos functions
+# Todo JSON Initializer
+# Todo time interval selector (seconds, minutes, hours, days)
 class Focus:
     """Object around which satellite will  orbit"""
     name = ""
@@ -27,47 +27,60 @@ class Focus:
         except ValueError as e:
             print("An unaccepted variable type was entered, Focus requires Str, Float, Float\n", e)
 
+    def add_to_satellites(self, satellite: object):
+        self.satellite_list.append(satellite)
+
 
 class Satellite:
     """Satellite that will orbit"""
-    # Variables independent from Focus
-    name: str = ""
-    mass: float = 0.0
-    radius: float
-    # Variables dependent on Focus
-    focus = None
-    distance_to_focus: float = 0.0
-    real_distance: float = 0.0
-    # Calculated variables
-    velocity: float = 0.0
-    period: float = 0.0
-    angular_velocity: float = 0.0
 
-    def __init__(self, name: str, mass: float, radius: float = 0.0):
+    # # Variables independent from Focus
+    # name: str = ""
+    # mass: float = 0.0
+    #
+    # # Variables dependent on Focus
+    # focus = None
+    # radius: float = 0.0
+    # # Calculated variables
+    # velocity: float = 0.0
+    # period: float = 0.0
+    # angular_velocity: float = 0.0
+
+    def __init__(self, name: str, mass: float, focus: Focus = None, radius: float = None, velocity: float = None,
+                 period: float = None, angular_velocity: float = None):
         """
         Initializer for Satellite
         :param name: Str
         :param mass: Float
-        :param radius: Float
         """
         try:
+            # Independent of focus
             self.name = name
             self.mass = mass
+            # Dependent of focus
+            self.focus = focus
+            if focus:
+                focus.add_to_satellites(self)
             self.radius = radius
-        except TypeError as e:
-            print("An unaccepted variable type was entered, Satellite requires Str, Float, Float\n", e)
+            #     Calculated from focus
+            self.velocity = velocity
+            self.period = period
+            #     Calculated from velocity and radius
+            self.angular_velocity = angular_velocity
 
-    def set_focus(self, focus: Focus, distance_to_focus: float):
+        except TypeError as e:
+            print("An unaccepted variable type was entered, Satellite requires Str, Float\n", e)
+
+    def set_focus(self, focus: Focus, radius: float):
         """
-        Sets focus and distance to focus, calculates 'real_distance'
+        Sets focus and radius
         :rtype: None
         :param focus: Focus
-        :param distance_to_focus: float
+        :param radius: float
         """
         self.focus = focus
-        self.distance_to_focus = distance_to_focus
-
-        self.real_distance = self.focus.radius + self.radius + self.distance_to_focus
+        focus.add_to_satellites(self)
+        self.radius = radius
 
     def calculate_velocity(self) -> float:
         """
@@ -78,7 +91,7 @@ class Satellite:
         """
 
         try:
-            self.velocity = ((gravitational_constant * self.focus.mass) / self.real_distance) ** (1 / 2)
+            self.velocity = ((gravitational_constant * self.focus.mass) / self.radius) ** (1 / 2)
         except TypeError as e:
             print(e)
         return self.velocity
@@ -91,7 +104,7 @@ class Satellite:
         :rtype: Float
         """
         try:
-            self.period = (2 * pi * self.real_distance) / self.velocity
+            self.period = (2 * pi * self.radius) / self.velocity
         except Exception as e:
             print("Exception!", e)
 
@@ -105,13 +118,13 @@ class Satellite:
         :return: Angular velocity
         """
         try:
-            self.angular_velocity = self.velocity / self.real_distance
+            self.angular_velocity = self.velocity / self.radius
         except Exception as e:
             print(e)
 
         return self.angular_velocity
 
-    @lru_cache(maxsize=int(period) + 1)
+    @lru_cache(maxsize=None)
     def angle_to_x(self, angle: float) -> float:
         """
         Calculates x coordinate using the following formula:
@@ -120,9 +133,10 @@ class Satellite:
         :param angle: float radians
         :return: x-coordinate
         """
-        return cos(angle) * self.real_distance
+        # print(angle)
+        return cos(angle) * self.radius
 
-    @lru_cache(maxsize=int(period) + 1)
+    @lru_cache(maxsize=None)
     def angle_to_y(self, angle: float) -> float:
         """
         Calculates y coordinate using the following formula:
@@ -130,7 +144,8 @@ class Satellite:
         :param angle: float radians
         :return: y-coordinate
         """
-        return sin(angle) * self.real_distance
+        # print(angle)
+        return sin(angle) * self.radius
 
     def angle_to_coordinates(self, period=None) -> tuple:
         """Translates angular position to coordinates
@@ -151,9 +166,9 @@ class Satellite:
         return y_coords, x_coords
 
     def __str__(self) -> str:
-        return "Name: {}, Mass: {}, Radius: {}, Focus: {}, Distance to Focus: {}, Velocity: {}".format(self.name,
-                                                                                                       self.mass,
-                                                                                                       self.radius,
-                                                                                                       self.focus,
-                                                                                                       self.distance_to_focus,
-                                                                                                       self.velocity)
+        return "Name: {}, Mass: {}, Radius: {}, Focus: {}, Velocity: {}, Angular Velocity: {}".format(self.name,
+                                                                                                      self.mass,
+                                                                                                      self.radius,
+                                                                                                      self.focus,
+                                                                                                      self.velocity,
+                                                                                                      self.angular_velocity)
