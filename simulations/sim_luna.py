58aefc8f059fe7baf5666d8f2b7c6cf1fed600c4
Casper-Smet <casper.smet@student.hu.nl>
2019-06-22 14:50:46 +0200

    simulation scripts for moon and solar system

---
+++ simulations/sim_luna.py
@@ -0,0 +1,65 @@
+from classes import Focus, Satellite, json_satellite_construct
+import matplotlib;
+
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def animate(i):
+    # print(i)
+    ax1.clear()
+
+    ax1.set_ylim([min(orbit[0]) * 3, max(orbit[0]) * 3])
+    ax1.set_xlim([min(orbit[1]) * 3, max(orbit[1]) * 3])
+
+    ax1.set_title("Moon orbiting around the earth, m h^-1, roughly 27 days")
+
+    ax1.scatter([0], [0], label='Earth')
+
+    x = orbit[0][i]
+    y = orbit[1][i]
+
+    ax1.plot(orbit[1], orbit[0], label='Orbit {} m '.format(moon.velocity * moon.period))
+    ax1.plot([x, 0], [y, 0], label='Radius {} m'.format(round((x ** 2 + y ** 2) ** 1 / 2), 2))
+
+    ax1.scatter(x, y, label='Moon')
+    ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
+               fancybox=True, shadow=True, ncol=5)
+
+
+if __name__ == '__main__':
+    # Code to generate orbits, uncomment this block to calculate it yourself:
+    earth = Focus("Earth", 5.972E24)
+    moon = Satellite("Luna", 7.34767309E22)
+    moon.time_interval = moon.time_interval * 24
+    moon.set_focus(earth, 384400E3)
+    moon.calculate_velocity()
+    print(int(moon.calculate_period() / moon.time_interval))
+    moon.calculate_angular_velocity()
+    print(moon)
+    # A gap shows up if you calculate per day, to avoid this the the moon is plotted over twice its period.
+    orbit = moon.calculate_orbit(moon.period * 2)
+    moon.to_json('json\Luna')
+
+    # If you are calculating the orbits yourself, comment the 4 following lines
+    # For per day:
+    # moon, foc1 = json_satellite_construct('json/Luna')
+
+    # For per hour:
+    # moon, foc1, = json_satellite_construct('json/moon-hours')
+
+    orbit = moon.orbit
+
+    fig = plt.figure(figsize=(10, 6))
+    ax1 = fig.add_subplot(1, 1, 1)
+    ax1.set_ylim([min(orbit[0]) * 3, max(orbit[0]) * 3])
+    ax1.set_xlim([min(orbit[1]) * 3, max(orbit[1]) * 3])
+    box = ax1.get_position()
+    ax1.set_position([box.x0 + box.width * 0.1, box.y0, box.width * 0.5, box.height / 6 * 5])
+    ax1.scatter(orbit[1], orbit[0])
+    ani = animation.FuncAnimation(fig, animate, interval=1, frames=len(orbit[0]))
+
+    plt.show()
+    # Uncomment the following line to save as GIF, make sure you've got imagemagick installed
+    # ani.save("orbits/moon.gif", writer='imagemagick', fps=30)
---
+++ simulations/sim_solar_system.py
@@ -0,0 +1,80 @@
+from classes import Focus, Satellite, json_satellite_construct
+from utility import lcm
+import matplotlib;
+
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def animate(i):
+    ax1.clear()
+    ax1.set_ylim([axis_range * -1.1, axis_range * 1.1])
+    ax1.set_xlim([axis_range * -1.1, axis_range * 1.1])
+
+    ax1.scatter([0], [0], label=sun.name)
+    for satellite in sun.satellite_list:
+        ang_pos = lambda_dict[satellite.name](i)
+        x, y = satellite.angular_position_to_coordinates(ang_pos)
+
+        ax1.scatter(x, y, label=satellite.name)
+        # ax1.plot([x, 0], [y, 0], label="{} Radius: {}".format(satellite.name, (x ** 2 + y ** 2) ** (1 / 2)))
+        ax1.plot([x, 0], [y, 0], label="{} Radius".format(satellite.name))
+
+    ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
+               fancybox=True, shadow=True, ncol=5)
+
+
+def calculate_orbit_system(focus):
+    satellites = focus.satellite_list
+    for sat in satellites:
+        sat.time_interval = 1 * 60 * 60 * 24
+        sat.calculate_velocity()
+        sat.calculate_period()
+        sat.calculate_angular_velocity()
+        print(sat)
+
+    lambda_dict = dict()
+
+    period = lcm(earth.period, mars.period)
+    # period = neptune.period
+    for sat in satellites:
+        lambda_dict[sat.name] = sat.angular_position_at_t()
+        # sat.calculate_orbit(period)
+        # sat.to_json("json/{}".format(sat.name))
+    return period / sat.time_interval, lambda_dict
+
+
+if __name__ == '__main__':
+    sun = Focus("Sun", 1.989E30)
+
+    # It is recommended to simulate mercury, venus, mars and earth separately from Jupiter, Saturn, Uranus and Neptune.
+    # The latter being so much further from the sun that you cannot properly differentiate between the earlier planets.
+    # The orbital periods of the latter also being substantially larger from the earlier (years instead of months)
+    mercury = Satellite("Mercury", 0.330E24, sun, 0.0579E12 + 2.440E6)
+    venus = Satellite("Venus", 4.87E24, sun, 0.1082E12 + 6.052E6)
+    earth = Satellite("Earth", 5.972E24, sun, 149600000000)
+    mars = Satellite("Mars", 0.642E24, sun, 0.228E12 + 3.390E6)
+
+    # jupiter = Satellite("Jupiter", 1900E24, sun, 0.7883E12 + 69.91E6)
+    # saturn = Satellite("Saturn", 568E24, sun, 1.427E12 + 58.2E6)
+    # uranus = Satellite("Uranus", 86.8E24, sun, 2.871E12 + 58.2E6)
+    # neptune = Satellite("Neptune", 102.4E24, sun, 4.498E12 + 24.6E6)
+
+    per, lambda_dict = calculate_orbit_system(sun)
+
+    periods = [x.period for x in sun.satellite_list]
+
+    axis_range = sun.largest_radius()
+
+    fig = plt.figure(figsize=(10, 6))
+    ax1 = fig.add_subplot(1, 1, 1)
+    box = ax1.get_position()
+    ax1.set_position([box.x0 + box.width * 0.1, box.y0, box.width * 0.5, box.height / 6 * 5])
+
+    ani = animation.FuncAnimation(fig, animate, interval=1, frames=int(max(periods) / mars.time_interval))
+
+    plt.show()
+
+    # Uncomment the following line to save as GIF, make sure you've got imagemagick installed
+    # ani.save("orbits/solarinner.gif", writer='imagemagick', fps=30)
