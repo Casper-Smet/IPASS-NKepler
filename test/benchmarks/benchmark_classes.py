a340a8d099d600c59a93f24fd239e010077f84e1
Casper-Smet <casper.smet@student.hu.nl>
2019-06-26 12:45:44 +0200

    Benchmarked origin functions

--- test/benchmarks/benchmark_classes.py
+++ test/benchmarks/benchmark_classes.py
@@ -1,20 +1,24 @@
 import cProfile
 from lib.classes import *
 import matplotlib.pyplot as plt
-# TODO add origin functions
+from datetime import datetime as dt
 
-# Profiled per day
+
+# Profiled per minute
 
 def calculate_orbit_system(focus):
     satellites = focus.satellite_list
     for sat in satellites:
-        sat.time_interval = 1 * 60   # minutes
+        sat.time_interval = 1 * 60  # minutes
         sat.calculate_velocity()
         sat.calculate_period()
         sat.calculate_angular_velocity()
         sat.calculate_orbit()
-        for i in range(10, 100):
-            sat.calculate_t_for_position([sat.orbit[0][i], sat.orbit[1][i]])
+        sat.set_origin(date1, [sat.orbit[0][1000], sat.orbit[1][1000]])
+        sat.calculate_orbit(from_known=True)
+        sat.update_origin(date2)
+        sat.calculate_orbit(from_known=True)
+
 
 if __name__ == '__main__':
     cp = cProfile.Profile()
@@ -24,6 +28,8 @@ if __name__ == '__main__':
     venus = Satellite("Venus", 4.87E24, sun, 0.1082E12 + 6.052E6)
     earth = Satellite("Earth", 5.972E24, sun, 149600000000)
     mars = Satellite("Mars", 0.642E24, sun, 0.228E12 + 3.390E6)
+    date1 = dt(2000, 5, 2)
+    date2 = dt(2019, 6, 25)
     calculate_orbit_system(sun)
     cp.disable()
     print("Orbits calculated per minute, one full orbit per satellite")
