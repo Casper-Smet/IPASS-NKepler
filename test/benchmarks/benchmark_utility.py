f82908ad698dc96c64b4502c985a1970968530d2
Casper-Smet <casper.smet@student.hu.nl>
2019-06-26 12:48:34 +0200

    Benchmarked time_difference

--- test/benchmarks/benchmark_utility.py
+++ test/benchmarks/benchmark_utility.py
@@ -1,12 +1,15 @@
 import cProfile
 from lib.utility import *
-# TODO add time_difference
+
 if __name__ == '__main__':
     cp = cProfile.Profile()
+    date1 = dt(2000, 5, 2)
+    date2 = dt(2016, 6, 26)
     cp.enable()
     for i in range(500):
         range_setter(780, 360)
         radians(67)
         degrees(6)
+        time_difference(date1, date2)
     cp.disable()
     cp.print_stats()
